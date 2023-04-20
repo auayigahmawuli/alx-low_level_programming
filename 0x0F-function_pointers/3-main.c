33d9958381a174b9f976408b114b88a34dc13907
root <Prosper Mawuli Auayigah>
2023-04-19 21:05:05 -0700

    funcpointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,54 +1,46 @@
+#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /**
- * main - Prints the opcodes of its main function.
- * @argc: number of arguments passed to the program
- * @argv: array of arguments passed to the function
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program
+ * @argv: An Array of pointer to the argument
  *
- * Return: 0 on success, 1 or 2 otherwise
- */
-
-void print_opcodes(char *a, int n)
-{
-	int i;
-
-	for (i = 0; i < n; i++)
-	{
-		printf("%.2hhx", a[i]);
-		if (i < n - 1)
-		{
-			printf(" ");
-		}
-	}
-	printf("\n");
-}
-
-/**
- * main - prints the opcodes of its own main function
- * @argc: integer
- * @argv: character
- *
- * Return: always 0.
+ * Return: Always 0.
  */
 
 int main(int argc, char *argv[])
 {
-	int ae;
+	int one, two, ans;
+	int (*res)(int, int);
+	char *get_op;
 
-	if (argc != 2)
+	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(1);
+		exit(98);
 	}
-	ae = atoi(argv[1]);
-	if (ae < 0)
+
+	one = atoi(argv[1]);
+	two = atoi(argv[3]);
+	get_op = argv[2];
+
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
-		exit(2);
+		exit(99);
 	}
-	print_opcodes((char *)&main, ae);
 
+	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	res = get_op_func(get_op);
+	ans = res(one, two);
+
+	printf("%d\n", ans);
 	return (0);
 }
