99a7ab333381837bd1784bd41b27359caaef7eb6
root <Prosper Mawuli Auayigah>
2023-05-08 22:34:59 -0700

    fileio

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+	int fd, W, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 
-	if (fd == -1 || w == -1)
+	if (fd == -1 || W == -1)
 			return (-1);
 
 	close(fd);
